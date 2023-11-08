8510b9a3ab11738a678931bcb08628e026001e25
nchafle1575 <nchafle@gmail.com>
2023-11-09 03:20:56 +0530

    first commit

---
+++ src/main/java/com/oneId/OneIdApplication.java
@@ -0,0 +1,13 @@
+package com.oneId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneId/OneIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
